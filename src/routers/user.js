8cddfc2529961ed3250b8b3079e189f413aae0cb
Jana Shyti <jshyti@eagles.bridgewater.edu>
2024-02-04 15:31:12 -0500

    newjana4

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,14 +1,19 @@
+const { sendVerificationEmail } = require('../emails/account.js')
 const express = require('express')
 const User = require('../models/user')
 
 const router = new express.Router()
 
+
 // Add a new user
 router.post('/user', async (req, res) => {
+  delete req.body.email_verified
+  delete req.body.tokens
   const user = new User(req.body)
 
   try {
     await user.save()
+    sendVerificationEmail(user.email, user.username)
     res.status(201).send(user)
   } 
   catch(error) {
